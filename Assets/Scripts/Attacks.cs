5750f2b80f4f440b11293953dbff13a84d8cb0e9
Taylor Zieman <Tzieman6451@gmail.com>
2017-03-22 11:14:22 -0500

    Start on Bash

--- Assets/Scripts/Attacks.cs
+++ Assets/Scripts/Attacks.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Attacks : MonoBehaviour {
 
-    public float atkRad;
-    public float atkDmg;
-    public float poundSpd;
+    public float pndRad;
+    public float pndDmg;
+    public float pndSpd;
 
+    public float bashRad;
+    public float bashDmg;
+    public float bashSpd;
+    
     public bool isAttacking;
     public bool pound;
-
+    public bool bash;
+    
+    HeadMovement moveScript;
     GroundCheck groundCheck;
     GameObject head;
     // Use this for initialization
     void Start()
     {
+        moveScript = GameObject.Find("Player").GetComponent<HeadMovement>();
         groundCheck = GameObject.Find("GroundCheck").GetComponent<GroundCheck>();
         head = GameObject.Find("Player");
         isAttacking = false;
@@ -25,11 +32,19 @@ public class Attacks : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if(pound == true || bash == true)
+        {    
+            isAttacking == true;   
+        }
         if ((Input.GetKey(KeyCode.Alpha1) || Input.GetKey(KeyCode.Keypad1)) && !isAttacking)
         {
             pound = true;
         }
 
+        if ((Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Keypad2)) && !isAttacking)
+        {
+            pound = true;
+        }
         Pound();
     }
 
@@ -37,7 +52,7 @@ public class Attacks : MonoBehaviour {
     {
         if(groundCheck.grounded == false && pound == true)
         {
-            head.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -poundSpd);
+            head.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -pndSpd);
         }
         if(groundCheck.grounded == true && pound == true)
         {
